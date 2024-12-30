8e6e54bc58e5770f289352e68884ee3a76a814c0
JoHassel <johannes.hassel@gmail.com>
2024-12-30 15:35:05 +0100

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,1 +1,10 @@
+// Initialize:
+int f_sine = 50;                         // Frequency of the generated sine wave
+int pwm_periods = 10;                    // PWM Periods per half-wave
+int f_pwm = pwm_periods*f_sine;          // PWM frequency
+int scaler = 10;                         // ratio between f_pwm and f_switch
+int f_switch = scaler * f_pwm;           // Switching frequency
+int cycles = 0;                          // iterated on every timer callback
+int fin = scaler * pwm_periods * 2;      // cycle number at the end of a sine wave
+
 
