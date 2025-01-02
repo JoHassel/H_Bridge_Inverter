5c58c78b8f83dd3c06fc35d6c1995ae514dea6a7
JoHassel <johannes.hassel@gmail.com>
2025-01-02 02:27:55 +0100

    Code debugged

--- Micropython/MP_main.py
+++ Micropython/MP_main.py
@@ -4,19 +4,19 @@ from math import sin, pi
 from machine import Timer
 
 # Initialize:
-f_sine = 50                         # Frequency of the generated sine wave
-pwm_periods = 10                    # PWM Periods per half-wave
-f_pwm = int(pwm_periods*f_sine)     # PWM frequency
-scaler = 10                         # ratio between f_pwm and f_switch
-f_switch = scaler * f_pwm           # Switching frequency
-cycles = 0                          # iterated on every timer callback
-fin = scaler * pwm_periods * 2      # cycle number at the end of a sine wave
+f_sine = 50                                     # Frequency of the generated sine wave
+pwm_periods = 12                               # PWM Periods per half-wave
+f_pwm = int(pwm_periods*f_sine)                 # PWM frequency
+scaler = 12                                     # ratio between f_pwm and f_switch
+f_switch = scaler * pwm_periods * f_sine *2     # Switching frequency
+cycles = 0                                      # iterated on every timer callback
+fin = scaler * pwm_periods * 2                  # cycle number at the end of a sine wave
 
 # Calculate Look-up table:
 r = range(1,pwm_periods+1)
 sinewave = sin
 lut = [int((scaler + (scaler*sin(2*pi*x/pwm_periods)))/2) for x in r]
-print(lut)
+# print(lut)
 
 # Activate pins: 
 g1 = Pin(16, Pin.OUT)
@@ -26,22 +26,22 @@ g2 = Pin(17, Pin.OUT)
 def clock_cb(timer):
     global cycles
     num = cycles % scaler                   # cycle number within a PWM period
-    i = int((cycles%100) / scaler)          # number of PWM period
-
+    
     if num == 0:
         if cycles == fin:
             cycles = 0        
         g1.high()
         g2.low()
-        print("Pins 1&4 HIGH, Pins 2&3 LOW")
+        # print("Pins 1&4 HIGH, Pins 2&3 LOW")
 
+    i = int(cycles / (scaler * 2))      # number of PWM period
     if num == (lut[i]):
         g1.low()
         g2.high()
-        print("Pins 1&4 LOW, Pins 2&3 HIGH")
+        # print("Pins 1&4 LOW, Pins 2&3 HIGH")
     
     cycles +=1
-    print(f"Callback, Cycle: {cycles}, Num: {num}, i: {i} Lut[i]: {lut[i]}")
+    # print(f"Callback, Cycle: {cycles}, Num: {num}, i: {i} Lut[i]: {lut[i]}")
     
 
 # Timer Initialization:
